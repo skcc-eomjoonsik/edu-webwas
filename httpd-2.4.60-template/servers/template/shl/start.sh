0d1f1cde7be00e4b19cc4bd2a603ead216494ab7
ServerAdmin <webwas@ip-172-31-100-1.ap-northeast-2.compute.internal>
2024-08-19 16:28:12 +0900

    2024-08-19

--- httpd-2.4.60-template/servers/template/shl/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SERVER_NAME=template
-
-/software/apache/bin/apachectl -f /software/apache/servers/${SERVER_NAME}/conf/httpd.conf -k start
--- httpd-2.4.60-template/shl/apache_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-### check_web.sh after start
-check_web.sh
